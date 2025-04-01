e120c91b60b37d02278e667bd1748bd37bb50b1d
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-01 18:02:57 -0300

    Arreglo de resources que no va mas en vite

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -4,22 +4,24 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        
-        
-        <title>Laravel</title>
+
+        <title>ElMensual</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
         <!-- FullCalendar Styles -->
         <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.min.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/common/main.min.css" rel="stylesheet" />
 
         <!-- Styles -->
-        @vite('resources/css/app.css')
+        @vite('css/app.css')
     </head>
     <body>
         <div id="app"></div>
-        @vite('resources/js/app.js')
+
+        <!-- Scripts -->
+        @vite('js/app.js')
     </body>
 </html>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,10 @@ export default defineConfig({
     base: "/build/",
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["css/app.css", "js/app.js"],
             refresh: true,
         }),
+
         vue(),
     ],
     build: {
