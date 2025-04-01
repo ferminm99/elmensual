8db2e3509a9438193e1d58d4d8deaeaab0248502
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-01 18:11:10 -0300

    Arreglo definitvo en vite creo?

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,8 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    base: "/build/",
-    plugins: [
-        laravel({
-            input: ["css/app.css", "js/app.js"],
-            refresh: true,
-        }),
-
-        vue(),
-    ],
+    base: "/",
+    plugins: [vue()],
     build: {
         outDir: "dist",
         rollupOptions: {
@@ -24,15 +17,15 @@ export default defineConfig({
         },
         chunkSizeWarningLimit: 1600,
     },
-    server: {
-        watch: {
-            usePolling: true, // Si estás trabajando en contenedores, esto puede ser necesario
-        },
-        host: "0.0.0.0", // Escuchar en todas las interfaces
-        port: 5173, // Forzar el puerto 5173
-        strictPort: true, // Asegurarse de que siempre use el puerto 5173
-        hmr: {
-            host: "localhost", // Hot Module Replacement (HMR) en localhost
-        },
-    },
+    // server: {
+    //     watch: {
+    //         usePolling: true, // Si estás trabajando en contenedores, esto puede ser necesario
+    //     },
+    //     host: "0.0.0.0", // Escuchar en todas las interfaces
+    //     port: 5173, // Forzar el puerto 5173
+    //     strictPort: true, // Asegurarse de que siempre use el puerto 5173
+    //     hmr: {
+    //         host: "localhost", // Hot Module Replacement (HMR) en localhost
+    //     },
+    // },
 });
