c5679b171b2212d77bd190f7ca965d5aafa83922
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-02 20:12:36 -0300

    arreglo no va withsession

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -16,25 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias('csrf', \App\Http\Middleware\VerifyCsrfToken::class);
         $middleware->stateful(); // <- clave para sesiones cross-site
     })
-    ->withSession(function () {
-        return [
-            'driver' => 'file',
-            'lifetime' => 120,
-            'expire_on_close' => false,
-            'encrypt' => false,
-            'files' => storage_path('framework/sessions'),
-            'connection' => null,
-            'table' => 'sessions',
-            'store' => null,
-            'lottery' => [2, 100],
-            'cookie' => 'elmensual_session',
-            'path' => '/',
-            'domain' => '.elmensual.vercel.app',
-            'secure' => true,
-            'http_only' => true,
-            'same_site' => 'none',
-        ];
-    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
