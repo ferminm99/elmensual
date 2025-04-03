aec3a5e718945ff26e459cbb3ff27e74a45dd601
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-03 15:58:20 -0300

    test 4

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,19 +1,19 @@
 <?php
 // config/cors.php
-// return [
-//     'paths' => ['*'],
-//     'allowed_methods' => ['*'],
-//     'allowed_origins' => ['https://elmensual.vercel.app'],
-//     'allowed_origins_patterns' => [],
-//     'allowed_headers' => [
-//         'X-Requested-With',
-//         'Content-Type',
-//         'Accept',
-//         'X-CSRF-TOKEN',
-//         'X-XSRF-TOKEN',
-//         'x-xsrf-token'
-//     ],
-//     'exposed_headers' => ['XSRF-TOKEN'],
-//     'max_age' => 0,
-//     'supports_credentials' => true,
-// ];
+return [
+    'paths' => ['*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://elmensual.vercel.app'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => [
+        'X-Requested-With',
+        'Content-Type',
+        'Accept',
+        'X-CSRF-TOKEN',
+        'X-XSRF-TOKEN',
+        'x-xsrf-token'
+    ],
+    'exposed_headers' => ['XSRF-TOKEN'],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
