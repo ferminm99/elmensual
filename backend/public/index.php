9e1849aa49f6d1f339d28f963f82c4bfe9e01826
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-03 14:29:41 -0300

    index modification

--- backend/public/index.php
+++ backend/public/index.php
@@ -9,6 +9,14 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: https://elmensual.vercel.app");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: *");
+    exit(0);
+}
+
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
