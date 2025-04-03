911659ecae754d9a9ced8927c0f86aabe5a17180
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-03 16:22:36 -0300

    session

--- backend/config/session.php
+++ backend/config/session.php
@@ -156,7 +156,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', '.elmensual.vercel.app'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
