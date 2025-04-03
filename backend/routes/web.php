1c4e14b1db9582daf45e90448ef0be83356a6964
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-03 13:02:10 -0300

    dd test

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -80,6 +80,8 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/check-auth', [LoginController::class, 'checkAuth']);
 
 
+dd(app()->make(\Illuminate\Contracts\Http\Kernel::class)->getMiddleware());
+
 //Localidades
 Route::get('/api/localidades', [LocalidadController::class, 'index']);
 Route::post('/localidad', [LocalidadController::class, 'store']);
