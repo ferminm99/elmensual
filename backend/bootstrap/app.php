4cc267d52b2416c951acac4a6b0b3b3605c636e4
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-02 21:42:58 -0300

    last try

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
         // 🔥 Forzar uso de tu propia clase CSRF
-        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->prepend(\Illuminate\Http\Middleware\VerifyCsrfToken::class);
         //$middleware->stateful(); // <- clave para sesiones cross-site
     })
     ->withExceptions(function (Exceptions $exceptions) {
