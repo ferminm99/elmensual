e8e44446ab9f5c14cd677b326d38edb542a8c405
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-04 17:42:38 -0300

    stateful

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -17,4 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->stateful() // 🔥 Agregá esto
+    ->create();
+    
