cf9f1f960b1ebc8190410200e9d486632beef7ca
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-04 18:04:22 -0300

    die test

--- backend/app/Http/Controllers/Auth/LoginController.php
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -14,6 +14,7 @@ class LoginController extends Controller
     {
         \Log::info('🚨 Entró al método login');
 
+        die('Login fue ejecutado');
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
