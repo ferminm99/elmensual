2c403c728084104bfa24bae67e180500f6367fd7
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-06 12:35:33 -0300

    welcome

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -6,21 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ElMensual</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <!-- FullCalendar Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.min.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/common/main.min.css" rel="stylesheet" />
-
-    {{-- ⚙️ Link al frontend local o producción --}}
-    @if (app()->environment('local'))
-        <script type="module" src="http://localhost:5173/resources/js/app.js"></script>
-    @else
-        <script type="module" src="/build/assets/app.js"></script>
-        <link rel="stylesheet" href="/build/assets/app.css">
-    @endif
+    <script type="module" src="/build/assets/app.js"></script>
+    <link rel="stylesheet" href="/build/assets/app.css">
 </head>
 <body>
     <div id="app"></div>
