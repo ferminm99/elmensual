9c51309842008b0191c6af463edcd66def0fc1f9
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-06 12:42:57 -0300

    blade

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ElMensual</title>
-
+    @vite('resources/js/app.js')
     <script type="module" src="/build/assets/app.js"></script>
     <link rel="stylesheet" href="/build/assets/app.css">
 </head>
