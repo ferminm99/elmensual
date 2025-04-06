72c14213fb194ab043d25d257e2d1613080d3385
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-06 19:42:53 -0300

    init

--- backend/init.sh
+++ backend/init.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+echo "SESSION_SAME_SITE=$SESSION_SAME_SITE" > .env
+echo "SESSION_SECURE_COOKIE=$SESSION_SECURE_COOKIE" >> .env
+echo "SESSION_DOMAIN=$SESSION_DOMAIN" >> .env
+echo "APP_ENV=$APP_ENV" >> .env
+echo "APP_DEBUG=$APP_DEBUG" >> .env
+echo "APP_URL=$APP_URL" >> .env
+echo "DB_CONNECTION=$DB_CONNECTION" >> .env
+echo "DB_HOST=$DB_HOST" >> .env
+echo "DB_PORT=$DB_PORT" >> .env
+echo "DB_DATABASE=$DB_DATABASE" >> .env
+echo "DB_USERNAME=$DB_USERNAME" >> .env
+echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+
+
 echo "🧼 Limpiando cache de Laravel"
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
-
-echo "🛠 Migrando base de datos (si no está migrada)"
-php artisan migrate --force
 
 echo "⚙️ Cacheando configuración"
 php artisan config:cache
