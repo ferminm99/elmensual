cecf7bfeb4b5c1938d5b5f5ac757bf0d38c75837
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-06 19:57:04 -0300

    test?

--- backend/init.sh
+++ backend/init.sh
@@ -2,29 +2,29 @@
 
 echo "🚀 Init start"
 
-# 🧬 Inyectar variables al .env manualmente
-echo "APP_ENV=$APP_ENV" > .env
-echo "APP_DEBUG=$APP_DEBUG" >> .env
-echo "APP_KEY=$APP_KEY" >> .env
-echo "APP_URL=$APP_URL" >> .env
+# # 🧬 Inyectar variables al .env manualmente
+# echo "APP_ENV=$APP_ENV" > .env
+# echo "APP_DEBUG=$APP_DEBUG" >> .env
+# echo "APP_KEY=$APP_KEY" >> .env
+# echo "APP_URL=$APP_URL" >> .env
 
-echo "DB_CONNECTION=$DB_CONNECTION" >> .env
-echo "DB_HOST=$DB_HOST" >> .env
-echo "DB_PORT=$DB_PORT" >> .env
-echo "DB_DATABASE=$DB_DATABASE" >> .env
-echo "DB_USERNAME=$DB_USERNAME" >> .env
-echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+# echo "DB_CONNECTION=$DB_CONNECTION" >> .env
+# echo "DB_HOST=$DB_HOST" >> .env
+# echo "DB_PORT=$DB_PORT" >> .env
+# echo "DB_DATABASE=$DB_DATABASE" >> .env
+# echo "DB_USERNAME=$DB_USERNAME" >> .env
+# echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 
-echo "SESSION_DRIVER=$SESSION_DRIVER" >> .env
-echo "SESSION_DOMAIN=$SESSION_DOMAIN" >> .env
-echo "SESSION_SECURE_COOKIE=$SESSION_SECURE_COOKIE" >> .env
-echo "SESSION_SAME_SITE=$SESSION_SAME_SITE" >> .env
+# echo "SESSION_DRIVER=$SESSION_DRIVER" >> .env
+# echo "SESSION_DOMAIN=$SESSION_DOMAIN" >> .env
+# echo "SESSION_SECURE_COOKIE=$SESSION_SECURE_COOKIE" >> .env
+# echo "SESSION_SAME_SITE=$SESSION_SAME_SITE" >> .env
 
-echo "SANCTUM_STATEFUL_DOMAINS=$SANCTUM_STATEFUL_DOMAINS" >> .env
-echo "XDG_CACHE_HOME=/tmp" >> .env # Para evitar warnings de Composer
+# echo "SANCTUM_STATEFUL_DOMAINS=$SANCTUM_STATEFUL_DOMAINS" >> .env
+# echo "XDG_CACHE_HOME=/tmp" >> .env # Para evitar warnings de Composer
 
-echo "✅ .env generado:"
-cat .env
+# echo "✅ .env generado:"
+# cat .env
 
 # 🧼 Limpiar cualquier cache previa
 php artisan config:clear
