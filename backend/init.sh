afee3610a45aa98d192beb184c8cd5d83681b328
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-06 15:21:27 -0300

    init

--- backend/init.sh
+++ backend/init.sh
@@ -5,5 +5,13 @@ php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
+echo "SESSION_SAME_SITE=none" >> .env
+echo "SESSION_DOMAIN=" >> .env
+echo "SESSION_SECURE_COOKIE=true" >> .env
+
+
+echo "⚙️ Cacheando config (usa .env ya cargado)"
+php artisan config:cache
+
 echo "🚀 Iniciando Laravel"
 php artisan serve --host=0.0.0.0 --port=8000
