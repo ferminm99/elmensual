bd1d7d8900a4a95733cd698f44d311eb4bd64559
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-07 20:23:01 -0300

    trye

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'login', 'logout', 'csrf-token', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'login', 'logout', 'csrf-token', 'sanctum/csrf-cookie','/csrf-token',],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://elmensual.vercel.app','VITE_APP_URL','http://localhost:5173','APP_URL','http://localhost:8000'],
     'allowed_origins_patterns' => [],
