a5b35bfa20f548dae78af054d36ea3664d49107b
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-07 10:07:07 -0300

    changes

--- backend/init.sh
+++ backend/init.sh
@@ -2,11 +2,9 @@
 
 echo "🚀 Init start"
 
-cp .env.production .env
-
 composer dump-autoload --optimize
 php artisan package:discover
 php artisan config:clear
 
-php artisan migrate --force
+# php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=8000
