99329d65cc1e21d6a283ac2d26253a54bacdb5d1
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-07 19:47:38 -0300

    init

--- backend/init.sh
+++ backend/init.sh
@@ -5,15 +5,15 @@ echo "🚀 Init start"
 # composer dump-autoload --optimize
 
 # ⚠️ Sobrescribir la config de sesión directamente
-echo "<?php return ['same_site' => 'none'];" > config/session.php
+# echo "<?php return ['same_site' => 'none'];" > config/session.php
 
 # Reinstalar sin scripts ni cache anticipada
-composer install --no-scripts --no-autoloader
-composer dump-autoload --optimize
+# composer install --no-scripts --no-autoloader
+# composer dump-autoload --optimize
 
 # Limpiar config
 php artisan config:clear
-php artisan config:cache
+# php artisan config:cache
 
 # Verificar que se tomó correctamente
 php artisan tinker --execute="echo '🧪 same_site: ' . config('session.same_site');"
