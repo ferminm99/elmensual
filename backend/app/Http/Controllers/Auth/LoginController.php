cfb236679edcecf2435678f9ab22c894b7a6c717
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-07 19:28:28 -0300

    lgin

--- backend/app/Http/Controllers/Auth/LoginController.php
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -16,7 +16,7 @@ class LoginController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+            // $request->session()->regenerate();
             return response()->json(['success' => true]);
         }
 
