2af9fc0938b29cd19f2528c82c670f379a2024f4
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-07 22:28:57 -0300

    ads

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -19,7 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class); 
         $middleware->validateCsrfTokens(except: [
             'api/login',  // <--- O la ruta que quieras deshabilitar
-            'api/'
+            'api/',
+            '/'
         ]);
         //
     })
