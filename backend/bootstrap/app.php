b850378ab72201f480921152bfde9da448962ffe
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-08 18:59:14 -0300

    app

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,8 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
-        $middleware->append(\Illuminate\Session\Middleware\StartSession::class);
-        $middleware->append(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
 
     })
     ->withExceptions(function (Exceptions $exceptions) {
