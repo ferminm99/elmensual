1c8acf4c7e39cbe1e409bacf7581c31446819406
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-08 14:35:07 -0300

    dockerfile

--- backend/init.sh
+++ backend/init.sh
@@ -2,16 +2,16 @@
 
 echo "🛠️ Iniciando contenedor..."
 
-# Reemplazar el puerto en nginx.conf
+# Reemplazar variable de entorno $PORT en nginx.conf
 export PORT=${PORT:-8080}
-envsubst '$PORT' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.final
-mv /etc/nginx/nginx.conf.final /etc/nginx/nginx.conf
+envsubst '$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Limpiar y cachear config
+# Laravel cache
 php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Iniciar nginx y php-fpm
+# Correr nginx + php-fpm
 php-fpm & nginx -g "daemon off;"
+
