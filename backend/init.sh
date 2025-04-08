55794336ae75208dedb3c8bf3f71c8f9d54a824a
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-08 14:25:43 -0300

    nginx

--- backend/init.sh
+++ backend/init.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
 
-echo "🚀 Init start"
+echo "🛠️ Iniciando contenedor..."
 
-# composer dump-autoload --optimize
-
-# ⚠️ Sobrescribir la config de sesión directamente
-# echo "<?php return ['same_site' => 'none'];" > config/session.php
-
-# Reinstalar sin scripts ni cache anticipada
-# composer install --no-scripts --no-autoloader
-# composer dump-autoload --optimize
-
-# Limpiar config
+# Limpiar y cachear config
 php artisan config:clear
-# php artisan config:cache
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
-# Verificar que se tomó correctamente
-# php artisan tinker --execute="echo '🧪 same_site: ' . config('session.same_site');"
-
-# php artisan migrate --force
-php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
+# Correr nginx y php-fpm
+php-fpm & nginx -g "daemon off;"
