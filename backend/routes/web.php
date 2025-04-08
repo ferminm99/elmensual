d2888b3a0f764c6b22a9597a83e59e1578e3a5c5
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-08 15:53:16 -0300

    try

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
+
+Route::get('/prueba-viva', fn() => '🔥 Laravel responde correctamente');
