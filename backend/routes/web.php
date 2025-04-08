d39e48ef515801c021d4f621cb2551824e6d0a8b
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-08 16:55:45 -0300

    web without blade

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return view('welcome');
-})->where('any', '.*');
+Route::get('/', function () {
+    return response()->json(['ok' => true]);
+});
+
 
 Route::get('/prueba-viva', fn() => '🔥 Laravel responde correctamente');
