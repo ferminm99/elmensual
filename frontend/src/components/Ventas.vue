5cbaac31526a954a931c13d73025a2cb4739992f
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-26 15:03:53 -0300

    try

--- frontend/src/components/Ventas.vue
+++ frontend/src/components/Ventas.vue
@@ -733,7 +733,7 @@ export default {
     },
     mounted() {
         this.loading = true;
-
+        localStorage.clear();
         window.addEventListener("notifyCacheChange", this.cacheListener);
 
         Promise.all([
